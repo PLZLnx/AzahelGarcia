7dc0e873fb655a417b335a08bac40e9111d14f84
Kevin <kevinxney@gmail.com>
2021-05-06 15:04:55 -0500

    Mi primer commit

---
+++ core/header.php
@@ -0,0 +1,69 @@
+<header>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script> 
+  <script type="text/javascript" href="js/header.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/header.css">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top" id="banner">
+    <div class="container">
+    <!-- Brand -->
+    <a class="navbar-brand" href="#"><span>Azahel García</span> Especialista en Redes Inalambricas</a>
+
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+          Planes de Internet</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li> 
+       <!-- Dropdown -->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+          ¿Quienes somos?
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Acerca de Nosotros</a>
+          <a class="dropdown-item" href="#">Contacto</a>
+          <a class="dropdown-item" href="#">Link 3</a>
+        </div>
+      </li>
+      </ul>
+    </div>
+    </div>
+  </nav>
+
+  <div class="banner">
+    <div class="container">
+    <div class="banner-text">
+    <div class="banner-heading">
+    Glad to see you here !
+    </div>
+    <div class="banner-sub-heading">
+    Here goes the secondary heading on hero banner
+    </div>
+    <button type="button" class="btn btn-warning text-dark btn-banner">Get started</button>
+    </div>
+    </div>
+  </div>
+  <!-- <section id="about">
+  <div class="container">
+    <div class="text-intro">
+    <h2>About Us</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+    </div>
+  </div>
+  </section> -->
+</header>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<script src="js/bootstrap.bundle.min.js" ></script>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="core/navbar.html">
+<link rel="stylesheet" type="text/css" href="core/header.php">
+
+
+<head>
+	<title>Azahel Garcia - Especialista en Redes Inalambricas</title>
+	<?php include('core/header.php') ?>
+	
+	
+</head>
+
+<body>
+
+</body>
+</html>
